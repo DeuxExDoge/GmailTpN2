4fa7d2f09decaac982a777a961e58eb63ed00c0a
DeuxExDoge <133285366+DeuxExDoge@users.noreply.github.com>
2023-10-08 17:02:48 -0300

    Changes2

---
+++ src/main/java/GestorCorreo/Mail.java
@@ -0,0 +1,23 @@
+package GestorCorreo;
+
+import java.util.ArrayList;
+
+public class Mail {
+
+    private String asunto;
+    private String mensaje;
+    private String remitente;
+    private ArrayList<String> para = new ArrayList<>();
+    
+
+    public Mail(String asunto, String mensaje,  String remitente, ArrayList<String> para){
+        this.asunto = asunto;
+        this.mensaje = mensaje;
+        this.remitente = remitente;
+        this.para = para;
+    }
+
+    public String getAsunto() {
+        return asunto;
+    }
+}
